d9608cf6cb71cc1ed2af74aec6b9f348b63fc671
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-06-20 18:46:31 -0700

    update 1st batch estimates

--- countdown1.js
+++ countdown1.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate1 = new Date("Jun 30, 2023").getTime();
+var countDownDate1 = new Date("Jun 26, 2023").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {

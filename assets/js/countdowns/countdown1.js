3ba3d32db6de1874a95f0bbc89d09f14115fb1cf
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-07-09 04:24:45 -0700

    Update countdown1.js

--- assets/js/countdowns/countdown1.js
+++ assets/js/countdowns/countdown1.js
@@ -1,8 +1,21 @@
-window.onload = function() {
-    var numAnim = new countUp.CountUp('countup1', 1414, { duration: 3 });
-    if (!numAnim.error) {
-      numAnim.start();
-    } else {
-      console.error(numAnim.error);
-    }
-  }
+function formatTime(value, label) {
+  return value + " " + label + (value === 1 ? "" : "s");
+}
+
+var weeks = 0;
+var days = 0;
+var hours = 0;
+var minutes = 0;
+
+function infiniteLoop() {
+  weeks = weeks + 1;
+
+  var countdown = `${formatTime(weeks, "week")}`
+
+  document.getElementById("countdown1").innerHTML = countdown
+
+  setTimeout(infiniteLoop, 10000);
+};
+
+
+infiniteLoop();
